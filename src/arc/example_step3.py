22a1aebb58885905a42681002249145daf6088f1
Joseph Gutenson <joseph.l.gutenson@erdc.dren.mil>
2024-12-09 08:36:52 -0700

    example_step3.py
    Added example script demonstrating how to run the updated version of ARC.

---
+++ src/arc/example_step3.py
@@ -0,0 +1,9 @@
+from arc import Arc
+
+#### STEP 3 Example ####
+# This is a path to the ARC input file you created in Step 3
+arc_input_file_path = ""
+
+arc = Arc(arc_input_file_path)
+arc.run() # Runs ARC
+
