fd74052503757c41e2418cd9790ba836fc0f2099
follumm <mike@follumhydro.com>
2024-12-22 05:41:05 -0700

    removed folder

--- build/lib/arc/__init__.py
+++
@@ -1,5 +0,0 @@
-from ._log import LOG
-from .arc import Arc
-
-__all__ = ['Arc']
-__version__ = '0.1.0'
--- build/lib/arc/example_step3.py
+++
@@ -1,9 +0,0 @@
-from arc import Arc
-
-#### STEP 3 Example ####
-# This is a path to the ARC input file you created in Step 3
-arc_input_file_path = ""
-
-arc = Arc(arc_input_file_path)
-arc.run() # Runs ARC
-
